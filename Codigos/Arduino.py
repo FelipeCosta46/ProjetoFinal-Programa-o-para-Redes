93b207f6f7af34ccc4b3021cb23e992407d4f2e3
FelipeCosta46 <51421214+FelipeCosta46@users.noreply.github.com>
2019-12-12 14:30:29 -0300

    Update Arduino.py

--- Codigos/Arduino.py
+++ Codigos/Arduino.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import threading
 import datetime
 import json
@@ -9,7 +8,7 @@ import requests
 
 ser = serial.Serial("/dev/ttyACM0")
 ser.flushInput()
-ip_servidor = "http://10.25.3.133:5000/"
+ip_servidor = "IP_MÁQUINA"
 
 try:
     r = requests.get(ip_servidor)
@@ -20,34 +19,31 @@ except:
 def envia_gas(valor):
     time.sleep(15)
     print("Enviando valor do gás...")
-    r = requests.post("{}api/inserir_gas/{}".format(ip_servidor, valor))
+    r = requests.post("{}api/ins_gas/{}".format(ip_servidor, valor))
 
 def envia_humidade(valor):
     time.sleep(20)
     print("Enviando valor da humidade...")
-    r = requests.post("{}api/inserir_humidade/{}".format(ip_servidor, valor))
+    r = requests.post("{}api/ins_humidade/{}".format(ip_servidor, valor))
 
 def envia_temperatura(valor):
     time.sleep(25)
     print("Enviando valor da temperatura...")
-    r = requests.post("{}api/inserir_temperatura/{}".format(ip_servidor, valor))
+    r = requests.post("{}api/ins_temperatura/{}".format(ip_servidor, valor))
 
-"""
-Lê os dados que o Arduino envia para a porta serial.
-"""
 while True:
     try:
-        ser_bytes = ser.readline()          # Leitura e decodificação dos bytes da porta serial
+        ser_bytes = ser.readline()          
         data = ser_bytes.decode("UTF-8")
 
-        data = data.split(" ")              # Facilita a extração do conteúdo
+        data = data.split(" ")              
 
         if len(data) == 3:
-            # Manda para o servidor no tempo estipulado
             t1 = threading.Thread(target = envia_gas, args = (data[2], )).start()
             t2 = threading.Thread(target = envia_humidade, args = (data[0], )).start()
             t3 = threading.Thread(target = envia_temperatura, args = (data[1], )).start()
             data = ""
+            
     except Exception as ex:
         pass
 
