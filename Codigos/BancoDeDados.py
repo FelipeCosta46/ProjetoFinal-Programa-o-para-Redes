6de97cd98cfe8f8065fe6b2ae119e6bead165823
FelipeCosta46 <51421214+FelipeCosta46@users.noreply.github.com>
2019-12-12 14:40:54 -0300

    Update BancoDeDados.py

--- Codigos/BancoDeDados.py
+++ Codigos/BancoDeDados.py
@@ -2,27 +2,18 @@ import sqlite3
 import datetime
 
 class BancoDeDados():
-    """
-    Faz a conexão com o banco de dados.
-    """
     def __init__(self):
         self.conn = sqlite3.connect("banco.db", check_same_thread = False)
 
-    """
-    Adiciona os dados de humidade na tabela Humidade juntamente com o horário.
-    """
-    def inserir_humidade(self, valor):
+    def ins_humidade(self, valor):
         cursor = self.conn.cursor()
         cursor.execute("INSERT INTO Humidade (horario, valor) VALUES (?, ?)",
                 (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), valor))
 
         self.conn.commit()
         return 200
-
-    """
-    Adiciona os dados de temperatura na tabela Temperatura juntamente com o horário.
-    """
-    def inserir_temperatura(self, valor):
+    
+    def ins_temperatura(self, valor):
         cursor = self.conn.cursor()
         cursor.execute("INSERT INTO Temperatura (horario, valor) VALUES (?, ?)",
                 (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), valor))
@@ -30,10 +21,7 @@ class BancoDeDados():
         self.conn.commit()
         return 200
 
-    """
-    Adiciona os dados de gás na tabela Gas juntamente com o horário.
-    """
-    def inserir_gas(self, valor):
+    def ins_gas(self, valor):
         cursor = self.conn.cursor()
         cursor.execute("INSERT INTO Gas (horario, valor) VALUES (?, ?)",
                 (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), valor))
@@ -41,10 +29,6 @@ class BancoDeDados():
         self.conn.commit()
         return 200
 
-    """
-    Seleciona os dados baseado nos critérios definidos pelo usuário.
-    Opções: apenas a humidade, apenas a temperatura, apenas o gás ou todas as anteriores.
-    """
     def selecionar_dados(self, humidade, temperatura, gas, de, ate):
         cursor = self.conn.cursor()
 
